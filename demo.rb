25e4775642cd8f7bc1377368594a13af0fe52efd
Load A <143745153+load-a@users.noreply.github.com>
2023-09-11 22:21:43 -0400

    Update 1

--- demo.rb
+++ demo.rb
@@ -50,14 +50,42 @@ end
 		operation
 	}
 
+
 #Page Divider 						< __d >			1
 #________________________________________
 # - title - 
 
+
 #Use string to poke				< __p > 		3
 puts "name: #{variable} ~ context" # POKE name
 
-#Subtitle									< __s > 		1
-# - - title 
 
+#Subtitle									< __s > 		1
+#_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+# * subtitle * 
+
+
+#Program Header						< __h >			4
+#________________________________________
+# Program Title
+#________________________________________
+#________________________________________
+# - Program Specifications - 
+#	Takes: 		arguments		: type
+#	Returns:	return_value	: type
+#________________________________________
+# - First Section - 
+
+
+#Notes										< __n >			2
+# Note: notes
+#		- notes
+
+
+#Method Inputs & Outputs	< __io >		2
+#	Takes: 		arguments : type
+#	Returns:	return_value : type
+
+#Brief Method Summary			< __b >			1
+#	Brief: This method does what?
 
